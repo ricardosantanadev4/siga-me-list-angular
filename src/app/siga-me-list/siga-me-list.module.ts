0634a8275501f9d565e5ac7cba44c44a73c53cc1
ricardosantanadev <ricardosantanadev@gmail.com>
2023-02-24 20:34:41 -0300

    update

---
+++ src/app/siga-me-list/siga-me-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SigaMeListRoutingModule } from './siga-me-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SigaMeListRoutingModule
+  ]
+})
+export class SigaMeListModule { }
